875ebefa16ef231c10c7e7c4be84a3f78f5ada4f
tdnam3 <tdnam3@cmcglobal.vn>
2025-02-18 10:03:18 +0700

    feat: config scanner camera

--- MyApp/Views/ScannerVCDelegate.swift
+++ MyApp/Views/ScannerVCDelegate.swift
@@ -12,7 +12,7 @@ protocol ScannerVCDelegate: AnyObject {
     func didFind(barcode: String)
 }
 
-final class ScannerVC: UIViewController {
+final class ScannerVC: UIViewController, AVCaptureMetadataOutputObjectsDelegate {
     let captureSession = AVCaptureSession()
     var previewLayer: AVCaptureVideoPreviewLayer?
     weak var scannerDelegate: ScannerVCDelegate?
@@ -28,11 +28,41 @@ final class ScannerVC: UIViewController {
     
     private func setupCaptureSession() {
         guard let backCameraDevice = AVCaptureDevice.default(for: .video) else { return }
+        let videoInput: AVCaptureDeviceInput
         do {
-            let input = try AVCaptureDeviceInput(device: backCameraDevice)
-            captureSession.addInput(input)
+            try videoInput = AVCaptureDeviceInput(device: backCameraDevice)
+            if(captureSession.canAddInput(videoInput)){
+                captureSession.addInput(videoInput)
+            } else { return }
+            
         } catch {
             print("Failed to create AVCaptureDeviceInput \(error)")
         }
+        
+        
+        let metadataOutput = AVCaptureMetadataOutput()
+        if(captureSession.canAddOutput(metadataOutput)){
+            captureSession.addOutput(metadataOutput)
+            
+            metadataOutput.setMetadataObjectsDelegate(self, queue: .main)
+            metadataOutput.metadataObjectTypes = [.qr, .ean8, .ean13]
+        } else { return }
+        
+        previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        previewLayer?.videoGravity = .resizeAspectFill
+        previewLayer?.frame = view.layer.bounds
+        view.layer.addSublayer(previewLayer!)
+        
+        captureSession.startRunning()
+    }
+    
+    func metadataOutput(_ output: AVCaptureMetadataOutput, didOutput metadataObjects: [AVMetadataObject], from connection: AVCaptureConnection) {
+        guard let metadataObject = metadataObjects.first as? AVMetadataMachineReadableCodeObject else { return }
+        
+        guard let barcodeStringValue = metadataObject.stringValue else { return }
+        
+        scannerDelegate?.didFind(barcode: barcodeStringValue)
+        
+        print("Scanned barcode: \(barcodeStringValue)")
     }
 }
